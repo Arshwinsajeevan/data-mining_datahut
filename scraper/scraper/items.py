0cc830fa01e50baa4bddc2331dc9ed2cdc6e2f45
Arshwinsajeevan <arshwin619@gmailcom>
2025-11-13 14:53:29 +0530

    Add AgentItem schema

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -1,12 +1,19 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
-class ScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class AgentItem(scrapy.Item):
+    profile_url = scrapy.Field()
+    first_name = scrapy.Field()
+    middle_name = scrapy.Field()
+    last_name = scrapy.Field()
+    address = scrapy.Field()
+    city = scrapy.Field()
+    state = scrapy.Field()
+    zipcode = scrapy.Field()
+    image_url = scrapy.Field()
+    title = scrapy.Field()
+    website = scrapy.Field()
+    office_phone_numbers = scrapy.Field()
+    agent_phone_numbers = scrapy.Field()
+    social = scrapy.Field() 
+    languages = scrapy.Field()
+    description = scrapy.Field()
